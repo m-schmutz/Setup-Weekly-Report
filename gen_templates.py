ad3432e2a00f06ea3e9a2f2f9754d5270284aa0b
m-schmutz <marschm131@gmail.com>
2022-11-07 14:24:44 -0800

    new week selection system

--- gen_templates.py
+++ gen_templates.py
@@ -6,10 +6,6 @@ from os import mkdir
 from os.path import exists
 from sys import exit
 
-def datestr(year:int, weeknum:int, daynum:int) -> str:
-    return datetime.fromisocalendar(year, weeknum, daynum).strftime("%B %-d, %Y")
-
-
 # commands for unzipping the templates
 UNZIP_DAILY = ('unzip', 'daily.zip')
 UNZIP_WEEKLY = ('unzip', 'weekly.zip')
@@ -20,7 +16,45 @@ RM_DAILY = ('rm', 'daily.html')
 RM_WEEKLY = ('rm', 'weekly.html')
 
 
-# # prompt user for new directory name
+# patterns for date formatting
+FULL = '%B %-d, %Y'
+FILE = '%-m-%-d'
+
+
+# return the string representation of the date passed
+def datestr(year:int, weeknum:int, daynum:int, pattern:str) -> str:
+    return datetime.fromisocalendar(year, weeknum, daynum).strftime(pattern)
+
+
+# return the week that the user wants to use for the templates
+def week_info() -> tuple:
+    # get the current year and week number
+    year, weeknum = datetime.now().isocalendar()[:2]
+
+    # generate info for current and following week
+    curr_week = tuple((datestr(year, weeknum, i, FULL), (datestr(year, weeknum, i, FILE))) for i in range(1, 6))
+    next_week = tuple((datestr(year, weeknum + 1, i, FULL), (datestr(year, weeknum + 1, i, FILE))) for i in range(1, 6))
+
+    # store as two options in a dictionary
+    options = {1: curr_week, 2: next_week}
+
+    # prompt user for what week they want to use
+    print('Select week to generate templates for:')
+    print(f'1: {curr_week[0][0]} - {curr_week[4][0]}')
+    print(f'2: {next_week[0][0]} - {next_week[4][0]}')
+    
+    # attempt to return the requested week
+    try:
+        week = int(input('> '))
+        print(f'{options[week] = }')
+
+    # quit if invalid option is given
+    except:
+        print(f'\033[31mERROR\033[0m: Input must be \'1\' or \'2\'')
+        exit()
+
+
+# prompt user for new directory name
 dir_name = str(input('Enter new directory name: '))
 
 
@@ -33,14 +67,9 @@ if exists(dir_name):
 # get the team name
 team_name = str(input('Enter team name: '))
 
-# get the current week number and year
-year, week_number = datetime.now().isocalendar()[:2]
 
-
-print('Select week to generate templates for:')
-print(f'1: {datestr(year, week_number, 1)} - {datestr(year, week_number, 5)}')
-print(f'2: {datestr(year, week_number + 1, 1)} - {datestr(year, week_number+1, 5)}')
-increment = int(input('> ')) - 1
+# get the week to be used for the templates
+week = week_info()
 
 
 # create the new directory
@@ -51,13 +80,6 @@ mkdir(dir_name)
 run(UNZIP_DAILY)
 run(UNZIP_WEEKLY)
 
-# create a tuple of the days of the week
-days_of_week = tuple(datestr(year, week_number+increment, i) for i in range(1, 6))
-
-
-# create a tuple of the file names for each html file
-date_file_names = tuple(datestr(year, week_number+increment, i) for i in range(1, 6))
-
 
 # open the 'daily.html' file and read its contents
 with open('daily.html', 'r') as dhtml:
@@ -65,8 +87,8 @@ with open('daily.html', 'r') as dhtml:
     # read in all contents of the file
     daily_html = dhtml.read()
     
-    # 
-    for date, file_name in zip(days_of_week, date_file_names):
+    # loop through each day of the week
+    for date, file_name in week:
         
         # substitute in the date and team name
         updated_html = sub(r'{DATE}', date, daily_html)
@@ -83,11 +105,11 @@ with open('weekly.html', 'r') as whtml:
     weekly_html = whtml.read()
 
     # substitute in Monday and Friday's date
-    updated_html = sub(r'{MON}', days_of_week[0], weekly_html)
-    updated_html = sub(r'{FRI}', days_of_week[4], updated_html)
+    updated_html = sub(r'{MON}', week[0][0], weekly_html)
+    updated_html = sub(r'{FRI}', week[4][0], updated_html)
 
     # save the updated html into a new file
-    with open(f'{dir_name}/{date_file_names[0]}_{date_file_names[4]}.html', 'w') as newfile:
+    with open(f'{dir_name}/{week[0][1]}_{week[4][1]}.html', 'w') as newfile:
         newfile.write(updated_html)
 
 
